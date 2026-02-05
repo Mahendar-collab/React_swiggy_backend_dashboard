42b530f87756cbeca5c77a8171602bb98a85b558
mahendar <mahendarn915@gmail.com>
2026-02-05 09:21:15 +0530

    backend url updated with render url

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:4000"
+//export const API_URL = "http://localhost:4000"
 
-//export const API_URL="https://backend-nodejs-swiggy-clone-0jl4.onrender.com"
+export const API_URL="https://backend-nodejs-swiggy-clone-0jl4.onrender.com"
